0f4cde74a4a837c36166002f8910f54718d7564d
nozwock <57829219+nozwock@users.noreply.github.com>
2026-03-05 10:49:24 +0530

    Add NoDeathPenalty mod
    Original: https://github.com/Baiker000/NoDeathPenaltyNineSols

---
+++ NoDeathPenalty/Plugin.cs
@@ -0,0 +1,108 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace NoDeathPenalty;
+
+public class PluginInfo
+{
+    public const string PLUGIN_GUID = "nozwock.NoDeathPenalty";
+    public const string PLUGIN_NAME = "No Death Penalty";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
+
+// Slightly modified version of https://github.com/Baiker000/NoDeathPenaltyNineSols
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    static Harmony harmony;
+
+    void Awake()
+    {
+        Logger = base.Logger;
+
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginInfo.PLUGIN_GUID);
+        foreach (var method in harmony.GetPatchedMethods())
+        {
+            Logger.LogInfo($"Patched method: {method.DeclaringType?.FullName}.{method.Name}");
+        }
+        if (harmony.GetPatchedMethods().Count() == 0)
+        {
+            Logger.LogError($"Failed to apply Harmony patches.");
+        }
+    }
+
+    void OnDestroy()
+    {
+        Logger.LogInfo($"Unloading plugin {PluginInfo.PLUGIN_GUID}");
+        harmony?.UnpatchSelf();
+    }
+
+    // https://stackoverflow.com/questions/77435863/using-harmony-to-patch-the-real-content-of-an-async-method-for-a-unity-game
+    static MethodBase GetAsyncMethod(System.Type type, string name)
+    {
+        var targetMethod = AccessTools.Method(type, name);
+        var stateMachineAttr = targetMethod.GetCustomAttribute<AsyncStateMachineAttribute>();
+        var moveNextMethod = AccessTools.Method(stateMachineAttr.StateMachineType, "MoveNext");
+        return moveNextMethod;
+    }
+
+    [HarmonyPatch]
+    class Patch_PlayerGamePlayData_PlayerDeathPenalty
+    {
+        static MethodBase TargetMethod()
+        {
+            return GetAsyncMethod(typeof(PlayerGamePlayData), nameof(PlayerGamePlayData.PlayerDeathPenalty));
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldloc_1
+                    && codes[i + 1].opcode == OpCodes.Ldc_I4_0
+                    && codes[i + 2].opcode == OpCodes.Call
+                    && codes[i + 2].operand.ToString().Contains("set_CurrentGold"))
+                {
+                    codes[i].opcode = OpCodes.Nop;
+                    codes[i + 1].opcode = OpCodes.Nop;
+                    codes[i + 2].opcode = OpCodes.Nop;
+                }
+                if (codes[i].opcode == OpCodes.Ldloc_1
+                    && codes[i + 1].opcode == OpCodes.Ldc_I4_0
+                    && codes[i + 2].opcode == OpCodes.Call
+                    && codes[i + 2].operand.ToString().Contains("set_CurrentExp"))
+                {
+                    codes[i].opcode = OpCodes.Nop;
+                    codes[i + 1].opcode = OpCodes.Nop;
+                    codes[i + 2].opcode = OpCodes.Nop;
+                }
+            }
+            return codes.AsEnumerable();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerDeadRecord))]
+    [HarmonyPatch("StorePlayerDataBeforeDead")]
+    class Patch_PlayerDeadRecord_StorePlayerDataBeforeDead
+    {
+        static void Postfix(PlayerDeadRecord __instance)
+        {
+            __instance.ContainEXP.CurrentValue = 0;
+            __instance.ContainMoney.CurrentValue = 0;
+        }
+    }
+}
+
